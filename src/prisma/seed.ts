07b3a573c908b8fad0507d4cacb1b948c89265f7
farukade <farukadekunle@gmail.com>
2024-06-22 09:57:09 +0100

    seed up

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,28 +1,17 @@
-import { GroupController } from "../controller/contact-group.controller";
 import { prisma } from "../utils/utils";
 import { exportedContacts } from "./data/contacts";
 
 async function main() {
-  let newGroups: any = [];
-
-  const newGroup = await GroupController.getOrCreateGroup({
-    name: "All Contacts",
-  });
-
-  newGroups.push(newGroup);
-
   for (const contact of exportedContacts) {
     const con = await prisma.contacts.create({
       data: contact,
     });
 
-    await prisma.contactGroups.createMany({
-      data: newGroups.map((g: any) => {
-        return { groupId: g.id, contactId: con.id };
-      }),
+    await prisma.contactGroups.create({
+      data: { groupId: 1, contactId: con.id },
     });
 
-    console.log(`CONTACT - ${con.id}: GROUP - ${newGroups[0].id}`);
+    console.log(`CONTACT - ${con.id}: GROUP - ${1}`);
   }
 }
 
